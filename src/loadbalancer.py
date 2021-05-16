b5742686bc405dfa0240e616c38be5418f2f378f
Hamza Sohail <hamza.sohail@carleton.ca>
2021-05-16 17:05:24 -0400

    Add balancing logic

--- src/loadbalancer.py
+++ src/loadbalancer.py
@@ -1,11 +1,26 @@
-from flask import Flask
+import random
+import requests
+
+from http import HTTPStatus
+from flask import Flask, request
 
 loadbalancer = Flask(__name__)
 
+DASHBOARD_SERVERS = ["localhost:8081", "localhost:8082"]
+STORE_SERVERS = ["localhost:9081", "localhost:9082"]
+
 
 @loadbalancer.route("/")
-def hello():
-  return "hello"
-
-
-
+def router():
+  host_header = request.headers["Host"]
+  
+  if host_header == "www.dashboard.store.com":
+    response = requests.get(f"http://{random.choice(DASHBOARD_SERVERS)}")
+    return response.content, response.status_code
+  
+  elif host_header == "www.store.com":
+    response = requests.get(f"http://{random.choice(STORE_SERVERS)}")
+    return response.content, response.status_code
+  
+  else:
+    return "Not Found", HTTPStatus.NOT_FOUND
