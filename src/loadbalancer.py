670f874180497ee7c97523ad7512a20db81e1316
Hamza Sohail <hamza.sohail@carleton.ca>
2021-05-19 20:30:37 -0400

    Add path based routing

--- src/loadbalancer.py
+++ src/loadbalancer.py
@@ -6,8 +6,8 @@ from flask import Flask, request
 
 loadbalancer = Flask(__name__)
 
-SERVICE_A = ["localhost:8081", "localhost:8082"]
-SERVICE_B = ["localhost:9081", "localhost:9082"]
+DOG_SERVERS = ["localhost:8081", "localhost:8082"]
+CAT_SERVERS = ["localhost:9081", "localhost:9082"]
 
 
 @loadbalancer.route("/")
@@ -15,12 +15,27 @@ def router():
   host_header = request.headers["Host"]
   
   if host_header == "www.service-a.com":
-    response = requests.get(f"http://{random.choice(SERVICE_A)}")
+    response = requests.get(f"http://{random.choice(DOG_SERVERS)}")
     return response.content, response.status_code
   
   elif host_header == "www.service-b.com":
-    response = requests.get(f"http://{random.choice(SERVICE_B)}")
+    response = requests.get(f"http://{random.choice(CAT_SERVERS)}")
     return response.content, response.status_code
   
   else:
     return "Not Found", HTTPStatus.NOT_FOUND
+
+
+@loadbalancer.route("/dogs")
+def dogs():
+  response = requests.get(f"http://{random.choice(DOG_SERVERS)}")
+  return response.content, response.status_code
+
+
+@loadbalancer.route("/cats")
+def cats():
+  response = requests.get(f"http://{random.choice(CAT_SERVERS)}")
+  return response.content, response.status_code
+
+
+
